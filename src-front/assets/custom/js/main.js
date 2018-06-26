f939bacb072a08b6f6da382d15ccf211da7036ec
Igor Ivanov <drkierkegor@gmail.com>
2018-06-26 19:58:28 +0300

    sm scss updt: +product-item component

--- src-front/assets/custom/js/main.js
+++ src-front/assets/custom/js/main.js
@@ -3,3 +3,11 @@
 module.exports = function(msg) {
   alert(`Welcome ${msg}`);
 }
+
+let productItemOverlay = $(".overlay-block");
+
+$(".product-item .img-wrapper").hover(function() {
+  $(productItemOverlay).animate({top: "0px"}, 500);
+}, function() {
+  $(productItemOverlay).animate({top: "-18rem"}, 500);
+});
