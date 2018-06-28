5cd4dcbe7ff371f093a4177e58a6e7b0e2e752d5
Igor Ivanov <drkierkegor@gmail.com>
2018-06-28 15:36:56 +0300

    sm ui updt: +sidebar menu

--- src-front/assets/custom/js/main.js
+++ src-front/assets/custom/js/main.js
@@ -4,6 +4,7 @@ module.exports = function(msg) {
   alert(`Welcome ${msg}`);
 }
 
+
 // product item overlay
 
 let productItemOverlay = $(".overlay-block");
@@ -14,6 +15,18 @@ $(".product-item .img-wrapper").hover(function() {
   $(this).find(productItemOverlay).animate({top: "-18rem"}, 500);
 });
 
+
 // sidebar menu
 
+let sidebarInnerWidth = $(".sidebar-menu").innerWidth();
 
+$(".dropdown-sidebar-menu").css("left", sidebarInnerWidth);
+$(".dropdown-sidebar-menu").css("width", sidebarInnerWidth);
+
+$(".has-dropdown").hover(function() {
+  $(this).children(".link").addClass("current");
+  $(".dropdown-sidebar-menu").show();
+}, function() {
+  $(this).children(".link").removeClass("current");
+  $(".dropdown-sidebar-menu").hide();
+});
